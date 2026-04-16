6cb9023c1dcc7a0174744e3c6d95d2357711268d
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-04-16 16:55:10 +0200

    Optimize loop conditions in divisor calculation
    Change loop conditions to use '< N' instead of '<= N' for efficiency.

--- math/divisors.cpp
+++ math/divisors.cpp
@@ -14,8 +14,8 @@ vector<ll> get_divisors(ll n) {
 
 //seive for nuber of divisors for all numbers
 vector<int> sum(N + 1, 0);
-for (int i = 1; i <= N; i++) {
-    for (int j = i; j <= N; j += i) {
+for (int i = 1; i < N; i++) {
+    for (int j = i; j < N; j += i) {
         sum[j] += i;
     }
 }
