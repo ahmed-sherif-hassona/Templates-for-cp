a52c1f2f49e8e1989b9273a2e56269df4e065048
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-04-16 16:42:16 +0200

    Implement function to get divisors of a number

---
+++ math/divisors.cpp
@@ -0,0 +1,13 @@
+vector<ll> get_divisors(ll n) {
+  vector<ll> divs;
+  for (int i = 1; 1LL * i * i <= n; i++) {
+    if (n % i == 0) {
+      divs.push_back(i);
+      if (i != n / i) {
+        divs.push_back(n / i);
+      }
+    }
+  }
+  sort(divs.begin(), divs.end());
+  return divs;
+}
