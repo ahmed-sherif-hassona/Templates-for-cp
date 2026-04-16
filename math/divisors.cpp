1a3b4e060cd13b0ca678afae5f79ebf4c2dd0627
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-04-16 16:51:02 +0200

    Update divisors.cpp

--- math/divisors.cpp
+++ math/divisors.cpp
@@ -11,3 +11,11 @@ vector<ll> get_divisors(ll n) {
   sort(divs.begin(), divs.end());
   return divs;
 }
+
+//seive for nuber of divisors for all numbers
+vector<int> sum(N + 1, 0);
+for (int i = 1; i <= N; i++) {
+    for (int j = i; j <= N; j += i) {
+        sum[j] += i;
+    }
+}
