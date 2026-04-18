6d1c91c8c560768d94fcc7ce850975d245c5f3ba
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-04-18 18:35:27 +0200

    Refactor DSU implementation with vector support
    Refactor DSU structure to use vectors and improve functionality.

--- graph/DSU.cpp
+++ graph/DSU.cpp
@@ -1,47 +1,42 @@
-struct dsu
-{
-    int parent[N],group[N];
-    dsu()
-    {
-        for(int i=0;i<N;i++)
-        {
-            parent[i]=i;
-            group[i]=1;
-        }
+
+struct dsu {
+    int sz;
+    vector<int>parant,groub,mx,mn;
+    dsu(int n) {
+        sz=n;
+        parant=vector<int>(n+1);
+        groub=vector<int>(n+1,1);
+        mx=vector<int>(n+1);
+        mn=vector<int>(n+1);
+        iota(parant.begin(),parant.end(),0);
+        iota(mx.begin(),mx.end(),0);
+        iota(mn.begin(),mn.end(),0);
     }
-    int find(int i)
-    {
-        if(parent[i]==i)
-        {
-            return i;
-        }
-        return parent[i]= find(parent[i]);
+    int find(int node) {
+        if (parant[node]==node)return node;
+        return find(parant[node]);
     }
-    bool samegroup(int x,int y)
-    {
-        return find(x)==find(y);
+    void marge(int u,int v) {
+        int leader1=find(u);
+        int leader2=find(v);
+        if (leader1==leader2)return;
+        if (groub[leader1]>groub[leader2])swap(leader1,leader2);
+        parant[leader1]=leader2;
+        mx[leader2]=max(mx[leader1],mx[leader2]);
+        mn[leader2]=min(mn[leader1],mn[leader2]);
+        groub[leader2]+=groub[leader1];
     }
-    void merge(int x,int y)
-    {
-        int leader1=find(x);
-        int leader2=find(y);
-        if(leader1==leader2)
-        {
-            return;
-        }
-        if(group[leader1]>group[leader2])
-        {
-            parent[leader2]=leader1;
-            group[leader1]+=group[leader2];
-        }
-        else
-        {
-            parent[leader1]=leader2;
-            group[leader2]+=group[leader1];
-        }
+    bool samegroub(int u,int v) {
+        return find(u)==find(v);
     }
-    int getsize(int x)
-    {
-        return group[find(x)];
+    int getmin(int node) {
+        return mn[find(node)];
     }
+    int getmax(int node) {
+        return mx[find(node)];
+    }
+    int getsize(int node) {
+        return groub[find(node)];
+    }
+
 };
