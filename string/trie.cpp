ceaa7949251e0c2dbc08f318e265227a2e972855
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-05-18 15:53:42 +0300

    Update trie.cpp

--- string/trie.cpp
+++ string/trie.cpp
@@ -1,188 +1,129 @@
-const int N = 1e5 + 5;
-
 struct Trie{
-
-    multiset<int> level[N];
-
     struct Node{
+        Node*child[26];
+        int IsEnd,Prefix;
+        Node(){
+            memset(child,0,sizeof child);
+            IsEnd=Prefix=0;
+        }
+    };
+    Node*root=new Node();
+    void insert(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)
+            {
+                cur->child[idx]=new Node();
+            }
+            cur=cur->child[idx];
+            cur->Prefix++;
+        }
+        cur->IsEnd++;
+    }
+    bool SearchWord(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)return 0;
+            cur=cur->child[idx];
+        }
+        return cur->IsEnd;
+    }
+    int CountWord(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)return 0;
+            cur=cur->child[idx];
+        }
+        return cur->IsEnd;
+    }
+    int CountPrefix(string &s)
+    {
+        Node*cur=root;
+        for(auto it:s)
+        {
+            int idx=it-'a';
+            if(cur->child[idx]==0)return 0;
+            cur=cur->child[idx];
+        }
+        return cur->Prefix;
+    }
+};
 
+
+//----------------------------------------------------------------------------------------------
+
+
+struct Trie {
+
+    struct Node {
         Node* child[26];
+        int Prefix, IsEnd, depth;
 
-        int IsEnd, Prefix, depth;
-
-        Node(int d = 0){
-
+        Node(int d = 0) {
             memset(child, 0, sizeof child);
-
-            IsEnd = Prefix = 0;
-
+            Prefix = IsEnd = 0;
             depth = d;
         }
     };
 
     Node* root = new Node();
 
-    void insert(string &s)
+    multiset<int> level[N];
+
+    void insert(const string &s)
     {
         Node* cur = root;
 
-        for(int i = 0; i < s.size(); i++)
+        for (int i = 0; i < (int)s.size(); i++)
         {
             int idx = s[i] - 'a';
 
-            if(cur->child[idx] == 0)
+            if (!cur->child[idx])
             {
                 cur->child[idx] = new Node(i + 1);
-
                 level[i + 1].insert(0);
             }
 
             cur = cur->child[idx];
 
-            level[cur->depth].erase(
-                level[cur->depth].find(cur->Prefix)
-            );
-
+            level[cur->depth].erase(level[cur->depth].find(cur->Prefix));
             cur->Prefix++;
-
             level[cur->depth].insert(cur->Prefix);
         }
 
         cur->IsEnd++;
     }
 
-    bool SearchWord(string &s)
+    void erase(const string &s)
     {
         Node* cur = root;
 
-        for(auto it : s)
+        for (int i = 0; i < (int)s.size(); i++)
         {
-            int idx = it - 'a';
-
-            if(cur->child[idx] == 0)
-                return 0;
+            int idx = s[i] - 'a';
 
             cur = cur->child[idx];
+
+            level[cur->depth].erase(level[cur->depth].find(cur->Prefix));
+            cur->Prefix--;
+            level[cur->depth].insert(cur->Prefix);
         }
 
-        return cur->IsEnd;
-    }
-
-    int CountWord(string &s)
-    {
-        Node* cur = root;
-
-        for(auto it : s)
-        {
-            int idx = it - 'a';
-
-            if(cur->child[idx] == 0)
-                return 0;
-
-            cur = cur->child[idx];
-        }
-
-        return cur->IsEnd;
-    }
-
-    int CountPrefix(string &s)
-    {
-        Node* cur = root;
-
-        for(auto it : s)
-        {
-            int idx = it - 'a';
-
-            if(cur->child[idx] == 0)
-                return 0;
-
-            cur = cur->child[idx];
-        }
-
-        return cur->Prefix;
-    }
-
-    bool IsEmpty(Node* node)
-    {
-        for(int i = 0; i < 26; i++)
-        {
-            if(node->child[i])
-                return false;
-        }
-
-        return true;
-    }
-
-    bool Delete(Node* cur, string &s, int depth = 0)
-    {
-        if(cur == nullptr)
-            return false;
-
-        if(depth == s.size())
-        {
-            if(cur->IsEnd == 0)
-                return false;
-
-            cur->IsEnd--;
-
-            if(cur->IsEnd == 0 && IsEmpty(cur))
-            {
-                delete cur;
-                return true;
-            }
-
-            return false;
-        }
-
-        int idx = s[depth] - 'a';
-
-        if(Delete(cur->child[idx], s, depth + 1))
-        {
-            cur->child[idx] = nullptr;
-        }
-
-        else
-        {
-            if(cur->child[idx] == nullptr)
-                return false;
-
-            level[cur->child[idx]->depth].erase(
-                level[cur->child[idx]->depth].find(
-                    cur->child[idx]->Prefix
-                )
-            );
-
-            cur->child[idx]->Prefix--;
-
-            level[cur->child[idx]->depth].insert(
-                cur->child[idx]->Prefix
-            );
-        }
-
-        if(cur != root &&
-           cur->IsEnd == 0 &&
-           IsEmpty(cur))
-        {
-            delete cur;
-            return true;
-        }
-
-        return false;
-    }
-
-    void erase(string &s)
-    {
-        if(!SearchWord(s))
-            return;
-
-        Delete(root, s);
+        cur->IsEnd--;
     }
 
     int MaxPrefixAtDepth(int d)
     {
-        if(level[d].empty())
-            return 0;
-
+        if (level[d].empty()) return 0;
         return *level[d].rbegin();
     }
 };
