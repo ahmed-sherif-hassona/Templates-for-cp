7e1b883f4d47bd57ea4468f020b8ef0900efe019
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-05-26 02:05:35 +0300

    Update Hashing (substring).cpp

--- string/Hashing (substring).cpp
+++ string/Hashing (substring).cpp
@@ -1,60 +1,100 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-const int N=1e6+5,base1=127,base2=131,mod1=1e9+7,mod2=2e9+11;
-int n,q,pw1[N],pw2[N];pair<int,int>pre[N],suf[N];
-void init()
-{
-    pw1[0]=pw2[0]=1;
-    for(int i=1;i<N;i++)
-    {
-        pw1[i]=(1LL*pw1[i-1]*base1)%mod1;
-        pw2[i]=(1LL*pw2[i-1]*base2)%mod2;
+
+struct StringHashing {
+private:
+    using ll = long long;
+
+    const ll mod1 = 1e9 + 7;
+    const ll mod2 = 1e9 + 9;
+
+    ll base1, base2;
+    int n;
+
+    vector<ll> pw1, pw2;
+    vector<pair<ll,ll>> pre, suf;
+
+    void build_powers(int sz) {
+        pw1.assign(sz + 1, 1);
+        pw2.assign(sz + 1, 1);
+
+        for (int i = 1; i <= sz; i++) {
+            pw1[i] = (pw1[i - 1] * base1) % mod1;
+            pw2[i] = (pw2[i - 1] * base2) % mod2;
+        }
     }
-}
-void Hash(string&s,pair<int,int>arr[])
-{
-    int a=0,b=0;
-    for(int i=0;i<s.size();i++)
-    {
-        a=(1LL*a*base1)%mod1;
-        b=(1LL*b*base2)%mod2;
-        a=(a+s[i])%mod1;
-        b=(b+s[i])%mod2;
-        arr[i]={a,b};
+
+    void build_prefix(string &s, vector<pair<ll,ll>> &arr) {
+        ll a = 0, b = 0;
+
+        for (int i = 0; i < n; i++) {
+            a = (a * base1 + s[i]) % mod1;
+            b = (b * base2 + s[i]) % mod2;
+
+            arr[i] = {a, b};
+        }
     }
-}
-pair<int,int>get(int l,int r,pair<int,int>arr[])
-{
-    auto ret=arr[r];
-    int sz=r-l+1;
-    --l;
-    if(l>=0)
-    {
-        ret.first-=(1LL*arr[l].first*pw1[sz])%mod1;
-        if(ret.first<0) ret.first+=mod1;
-        ret.second-=(1LL*arr[l].second*pw2[sz])%mod2;
-        if(ret.second<0) ret.second+=mod2;
+
+public:
+
+    StringHashing(string s, ll b1 = 127, ll b2 = 131) {
+        base1 = b1;
+        base2 = b2;
+
+        n = s.size();
+
+        pw1.resize(n + 1);
+        pw2.resize(n + 1);
+
+        pre.resize(n);
+        suf.resize(n);
+
+        build_powers(n);
+
+        build_prefix(s, pre);
+
+        reverse(s.begin(), s.end());
+        build_prefix(s, suf);
+
+        reverse(s.begin(), s.end());
     }
-    return ret;
-}
-bool pal(int l,int r)
-{
-    return get(l,r,pre)==get(n-r-1,n-l-1,suf);
-}
-signed main()
-{
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    init();
-    string s;cin>>s;
-    n=s.size();
-    Hash(s,pre);
-    reverse(s.begin(),s.end());
-    Hash(s,suf);
-    reverse(s.begin(),s.end());
-    if(pal(0,n-1))
-    {
-        cout<<"YES\n";
+
+    pair<ll,ll> getHash(vector<pair<ll,ll>> &arr, int l, int r) {
+        auto res = arr[r];
+
+        if (l > 0) {
+            res.first =
+                (res.first - arr[l - 1].first * pw1[r - l + 1]) % mod1;
+
+            res.second =
+                (res.second - arr[l - 1].second * pw2[r - l + 1]) % mod2;
+        }
+
+        if (res.first < 0) res.first += mod1;
+        if (res.second < 0) res.second += mod2;
+
+        return res;
     }
-    return 0;
-}
+
+    bool isPalindrome(int l, int r) {
+        return getHash(pre, l, r)
+            == getHash(suf, n - r - 1, n - l - 1);
+    }
+
+    int count_unique_substrings() {
+        int ans = 0;
+
+        for (int len = 1; len <= n; len++) {
+
+            set<pair<ll,ll>> st;
+
+            for (int i = 0; i + len - 1 < n; i++) {
+                st.insert(getHash(pre, i, i + len - 1));
+            }
+
+            ans += st.size();
+        }
+
+        return ans;
+    }
+};
