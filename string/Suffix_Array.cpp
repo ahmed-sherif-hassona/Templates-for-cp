d18b1a9f85d4825169d29335ba0fccb22086d150
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-05-28 12:34:20 +0300

    Implement main function for SuffixArray
    Added main function to demonstrate SuffixArray usage.

---
+++ string/Suffix_Array.cpp
@@ -0,0 +1,127 @@
+struct SuffixArray {
+    string s;
+    int n;
+    vector<int> p, c, lcp, rankv;
+
+    SuffixArray(string s) {
+        this->s = s + '$';
+        n = this->s.size();
+        build_sa();
+        build_lcp();
+    }
+
+    void build_sa() {
+        p.resize(n);
+        c.resize(n);
+
+        // ---- k = 0 ----
+        vector<pair<char,int>> a(n);
+        for(int i = 0; i < n; i++)
+            a[i] = {s[i], i};
+
+        sort(a.begin(), a.end());
+
+        for(int i = 0; i < n; i++)
+            p[i] = a[i].second;
+
+        c[p[0]] = 0;
+        for(int i = 1; i < n; i++) {
+            if(a[i].first == a[i-1].first)
+                c[p[i]] = c[p[i-1]];
+            else
+                c[p[i]] = c[p[i-1]] + 1;
+        }
+
+        // ---- k > 0 ----
+        int k = 0;
+        while((1 << k) < n) {
+            for(int i = 0; i < n; i++) {
+                p[i] = (p[i] - (1 << k) + n) % n;
+            }
+
+            counting_sort();
+
+            vector<int> c_new(n);
+            c_new[p[0]] = 0;
+
+            int classes = 1;
+            for(int i = 1; i < n; i++) {
+                pair<int,int> cur = {c[p[i]], c[(p[i] + (1<<k)) % n]};
+                pair<int,int> prev = {c[p[i-1]], c[(p[i-1] + (1<<k)) % n]};
+
+                if(cur != prev) classes++;
+                c_new[p[i]] = classes - 1;
+            }
+
+            c = c_new;
+            k++;
+        }
+    }
+
+    void counting_sort() {
+        int n = p.size();
+        int classes = *max_element(c.begin(), c.end()) + 1;
+
+        vector<int> cnt(classes), pos(classes);
+
+        for(int x : c)
+            cnt[x]++;
+
+        for(int i = 1; i < classes; i++)
+            pos[i] = pos[i-1] + cnt[i-1];
+
+        vector<int> p_new(n);
+
+        for(int x : p) {
+            int i = c[x];
+            p_new[pos[i]++] = x;
+        }
+
+        p = p_new;
+    }
+
+    void build_lcp() {
+        rankv.resize(n);
+        for(int i = 0; i < n; i++)
+            rankv[p[i]] = i;
+
+        lcp.assign(n, 0);
+
+        int k = 0;
+        for(int i = 0; i < n; i++) {
+            if(rankv[i] == n - 1) {
+                k = 0;
+                continue;
+            }
+
+            int j = p[rankv[i] + 1];
+
+            while(i + k < n && j + k < n &&
+                  s[i + k] == s[j + k])
+                k++;
+
+            lcp[rankv[i]] = k;
+
+            if(k) k--;
+        }
+    }
+};
+
+
+//////////////////////////////////////////////////////
+
+int main() {
+    string s;
+    cin >> s;
+
+    SuffixArray sa(s);
+
+    // suffix array
+    for(int i = 0; i < sa.p.size(); i++)
+        cout << sa.p[i] << " ";
+    cout << "\n";
+
+    // lcp
+    for(int i = 0; i < sa.lcp.size(); i++)
+        cout << sa.lcp[i] << " ";
+}
