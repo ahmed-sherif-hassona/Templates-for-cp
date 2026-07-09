9a14dc1b7ea506e7e78c7cc3710046ddd284cfe9
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-07-09 11:43:50 +0300

    Implement Binary Trie for efficient bitwise operations

--- string/Binary_Trie.cpp
+++ string/Binary_Trie.cpp
@@ -46,3 +46,183 @@ struct BinaryTrie{
         return ret;
     }
 };
+
+
+
+//---------------------------------------------------------------------------------------
+
+struct BinaryTrie {
+    using ll = long long;
+
+    struct Node {
+        Node* child[2];
+        int cnt;
+
+        Node() {
+            child[0] = child[1] = nullptr;
+            cnt = 0;
+        }
+    };
+
+    Node* root;
+    int B;
+
+    BinaryTrie(int bits = 60) {
+        B = bits;
+        root = new Node();
+    }
+
+    void insert(ll x) {
+        Node* cur = root;
+
+        for (int i = B; i >= 0; i--) {
+            int b = (x >> i) & 1;
+
+            if (!cur->child[b])
+                cur->child[b] = new Node();
+
+            cur = cur->child[b];
+            cur->cnt++;
+        }
+    }
+
+    void remove(ll x) {
+        Node* cur = root;
+
+        for (int i = B; i >= 0; i--) {
+            int b = (x >> i) & 1;
+
+            if (!cur->child[b])
+                return;
+
+            cur = cur->child[b];
+            cur->cnt--;
+        }
+    }
+
+    bool contains(ll x) {
+        Node* cur = root;
+
+        for (int i = B; i >= 0; i--) {
+            int b = (x >> i) & 1;
+
+            if (!cur->child[b] || cur->child[b]->cnt == 0)
+                return false;
+
+            cur = cur->child[b];
+        }
+
+        return true;
+    }
+
+    ll maxXor(ll x) {
+        Node* cur = root;
+        ll ans = 0;
+
+        for (int i = B; i >= 0; i--) {
+
+            int b = (x >> i) & 1;
+
+            if (cur->child[b ^ 1] && cur->child[b ^ 1]->cnt > 0) {
+                ans |= (1LL << i);
+                cur = cur->child[b ^ 1];
+            }
+            else {
+                cur = cur->child[b];
+            }
+        }
+
+        return ans;
+    }
+
+    ll minXor(ll x) {
+        Node* cur = root;
+        ll ans = 0;
+
+        for (int i = B; i >= 0; i--) {
+
+            int b = (x >> i) & 1;
+
+            if (cur->child[b] && cur->child[b]->cnt > 0) {
+                cur = cur->child[b];
+            }
+            else {
+                ans |= (1LL << i);
+                cur = cur->child[b ^ 1];
+            }
+        }
+
+        return ans;
+    }
+
+    ll kthXor(ll x, int k) {
+        Node* cur = root;
+        ll ans = 0;
+
+        for (int i = B; i >= 0; i--) {
+
+            int b = (x >> i) & 1;
+
+            int same = 0;
+
+            if (cur->child[b])
+                same = cur->child[b]->cnt;
+
+            if (same >= k) {
+                cur = cur->child[b];
+            } else {
+                k -= same;
+                ans |= (1LL << i);
+                cur = cur->child[b ^ 1];
+            }
+        }
+
+        return ans;
+    }
+
+    int countLessThanK(ll x, ll k) {
+        Node* cur = root;
+        int ans = 0;
+
+        for (int i = B; i >= 0; i--) {
+
+            if (!cur)
+                break;
+
+            int xb = (x >> i) & 1;
+            int kb = (k >> i) & 1;
+
+            if (kb) {
+
+                if (cur->child[xb])
+                    ans += cur->child[xb]->cnt;
+
+                cur = cur->child[xb ^ 1];
+
+            } else {
+
+                cur = cur->child[xb];
+
+            }
+        }
+
+        return ans;
+    }
+
+    ll maxSubarrayXor(vector<ll>& a) {
+        BinaryTrie trie(B);
+
+        trie.insert(0);
+
+        ll pref = 0;
+        ll ans = 0;
+
+        for (ll x : a) {
+            pref ^= x;
+            ans = max(ans, trie.maxXor(pref));
+            trie.insert(pref);
+        }
+
+        return ans;
+    }
+};
