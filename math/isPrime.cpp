6ed97b0bcd0cb8ec83b1457ba0fee19a89866d69
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-07-18 15:00:35 +0300

    Implement isPrime function to check primality

---
+++ math/isPrime.cpp
@@ -0,0 +1,13 @@
+bool isPrime(long long n){
+
+    if(n<=1)
+        return false;
+
+    for(long long i=2;i*i<=n;i++){
+
+        if(n%i==0)
+            return false;
+    }
+
+    return true;
+}
