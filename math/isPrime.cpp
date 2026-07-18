32bfdea128ac6197611e07006ee7aef18a4b2afd
Ahmed_Sherif <ahmadshrif73@gmail.com>
2026-07-18 15:01:47 +0300

    Optimize isPrime function for even numbers

--- math/isPrime.cpp
+++ math/isPrime.cpp
@@ -3,7 +3,13 @@ bool isPrime(long long n){
     if(n<=1)
         return false;
 
-    for(long long i=2;i*i<=n;i++){
+    if(n==2)
+        return true;
+
+    if(n%2==0)
+        return false;
+
+    for(long long i=3;i<=n/i;i+=2){
 
         if(n%i==0)
             return false;
